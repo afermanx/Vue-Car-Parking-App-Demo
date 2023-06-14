0efaf32a8860c55ab16cf2b79345e779daa7ead1
Alex Ferman <alexjferman@gmail.com>
2023-06-13 22:29:46 -0400

    validation field

---
+++ src/components/ValidationError.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { defineProps } from "vue";
+const props = defineProps(["errors", "field"]);
+</script>
+
+<template>
+  <ul
+    v-if="props?.errors?.value?.[props.field]?.length"
+    class="flex flex-col gap-1 text-red-600"
+  >
+    <li v-for="message in props.errors.value[props.field]" :key="message">
+      {{ message }}
+    </li>
+  </ul>
+</template>
