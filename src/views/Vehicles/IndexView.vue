6451c998634b2ee9e3f37ad0f4a88bb5cc5f36e2
Alex Ferman <alexjferman@gmail.com>
2023-06-14 10:59:36 -0400

    list vehicle

--- src/views/Vehicles/IndexView.vue
+++ src/views/Vehicles/IndexView.vue
@@ -1,16 +1,49 @@
+<script setup>
+import { onMounted } from "vue";
+import { useVehicle } from "@/stores/vehicle";
+
+const store = useVehicle();
+
+onMounted(store.getVehicles);
+</script>
+
 <template>
-    <div class="flex flex-col w-full mx-auto md:w-96">
-      <h1 class="mb-4 text-2xl font-bold text-center">My vehicles</h1>
+  <div class="flex flex-col w-full mx-auto md:w-96">
+    <h1 class="mb-4 text-2xl font-bold text-center">My vehicles</h1>
 
-      <RouterLink
-        :to="{ name: 'vehicles.create' }"
-        class="w-full btn btn-primary"
+    <RouterLink
+      :to="{ name: 'vehicles.create' }"
+      class="w-full btn btn-primary"
+    >
+      Add vehicle
+    </RouterLink>
+
+    <div class="border-t h-[1px] my-6"></div>
+
+    <div class="flex flex-col gap-2">
+      <div
+        v-for="vehicle in store.vehicles"
+        :key="vehicle.id"
+        class="flex justify-between w-full p-2 bg-gray-100"
       >
-        Add vehicle
-      </RouterLink>
-
-      <div class="border-t h-[1px] my-6"></div>
-
-      <div>There will be our vehicles list.</div>
+        <div class="flex items-center w-full overflow-hidden">
+          <div class="text-xl plate">
+            {{ vehicle.plate_number }}
+          </div>
+          <div class="pl-2 font-normal text-gray-600 truncate grow">
+            {{ vehicle.description }}
+          </div>
+        </div>
+        <div class="flex gap-1">
+          <button type="button" class="text-sm btn btn-secondary">Edit</button>
+          <button
+            type="button"
+            class="text-sm text-white bg-red-600 btn hover:bg-red-500"
+          >
+            X
+          </button>
+        </div>
+      </div>
     </div>
-  </template>
+  </div>
+</template>
